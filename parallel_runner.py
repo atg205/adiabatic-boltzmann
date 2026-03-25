420b18b6c987450742b3f9cd60a4be01aa0e9e73
atg205 <p.hanu@mailbox.org>
2026-03-25 14:35:25 +0100

    velox

--- parallel_runner.py
+++ parallel_runner.py
@@ -36,11 +36,10 @@ SIZES = [4, 6, 8]
 N_NH_STEPS = 1  # generates N evenly-spaced steps ending at n_visible
 LEARNING_RATES = [0.1, 0.01]
 SAMPLERS = [
-    ("custom", "metropolis"),
-    ("dimod", "simulated_annealing"),
+    ("velox", "velox"),
 ]  # list of (sampler, method) pairs
 RBMS = ["full"]
-H_VALUES = [0.5,2.0]
+H_VALUES = [0.5,1.0,2.0]
 SEEDS = [1, 42]
 ITERATIONS = 300
 
